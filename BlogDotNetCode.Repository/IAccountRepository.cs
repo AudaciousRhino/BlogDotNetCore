198b637740b161864bab877cf7f5fe72e4cd7653
Vijay Patel <audaciousrhinomarketing@gmail.com>
2021-03-25 19:03:16 +0000

    Models project complete. Repository project created with Account Repository

---
+++ BlogDotNetCode.Repository/IAccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using BlogDotNetCode.Models.Account;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlogDotNetCode.Repository
+{
+    public interface IAccountRepository
+    {
+        public Task<IdentityResult> CreateAsync(ApplicationUserIdentity user, 
+            CancellationToken cancellationToken);
+
+        public Task<ApplicationUserIdentity> GetByUsernameAsync(string normalizedUsername, 
+            CancellationToken cancellationToken);
+    }
+}
