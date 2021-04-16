8215a3fb0c8614d5d0348718295841348f8f8cde
Vijay Patel <audaciousrhinomarketing@gmail.com>
2021-04-16 17:06:44 +0100

    Services project completed.

---
+++ BlogDotNetCode.Services/IPhotoService.cs
@@ -0,0 +1,14 @@
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace BlogDotNetCode.Services
+{
+    public interface IPhotoService
+    {
+        public Task<ImageUploadResult> AddPhotoAsync(IFormFile fromfile);
+
+        public Task<DeletionResult> DeletePhotoAsync(string publicId);
+    }
+}
---
+++ BlogDotNetCode.Services/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using BlogDotNetCode.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogDotNetCode.Services
+{
+    public interface ITokenService
+    {
+        public string CreateToken(ApplicationUserIdentity user);
+    }
+}
